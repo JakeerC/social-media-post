95a51c8f61fc19ad9035c29d5315c95c7125bd71
JakeerC <jakeerchilakala@gmail.com>
2024-05-05 18:04:47 +0530

    feat: protected routes , redirect to login page

---
+++ app/login/page.tsx
@@ -0,0 +1,17 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import AuthButtonClient from "../AuthButton.client";
+
+export default async function LogIn() {
+  const supabase = createServerComponentClient({ cookies: cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect("/");
+  }
+
+  return <AuthButtonClient session={session} />;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import AuthButtonServer from "./AuthButton.server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
 
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
   const { data: tweets } = await supabase.from("tweets").select();
+
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <AuthButtonServer />
